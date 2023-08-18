5d54694a1215f63929badf55dfe2fdda05a7fee6
riyatk2 <riyarajantk@gmail.com>
2023-08-18 10:15:19 +0530

    python basics

---
+++ day 1.py
@@ -0,0 +1,7 @@
+#syntax: the style in which we write the code.
+#commands: the unexecuted python codes.
+# the unexecuted codes or text start with hash.
+#variable : the place where data is stored temporarly.
+#eq: n="msc statistics"
+#number=2
+#naming rule:name must starts with alphabets or underscore. donot start the name with number.
