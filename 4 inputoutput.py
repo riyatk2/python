ddce70fbe6b8ef75cf62bd5e5e9b429f539906c4
riyatk2 <riyarajantk@gmail.com>
2023-08-21 10:27:46 +0530

    datastructures

---
+++ 4 inputoutput.py
@@ -0,0 +1,15 @@
+print("text")
+h=input("input a number")
+print(h)
+
+number1=int(input("enter first number"))
+number2=int(input("enter second number"))
+c=number1+number2
+print(c)
+
+num1=int(input("enter first number" ))
+num2=int(input("enter second number"))
+d=num1*num2
+print(d)
+
+
